d51ac987fe52e8fb20f4b1955e27d265599badca
Lukires <lukas.thomas.schilling@gmail.com>
2023-03-17 14:45:03 +0100

    Fixed implementation to work with the latest wayf certificates

--- WayfJwtConnector/Helpers/RsaHelper.cs
+++ WayfJwtConnector/Helpers/RsaHelper.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
 
 namespace WayfJwtConnector.Helpers
 {
@@ -14,7 +15,8 @@ namespace WayfJwtConnector.Helpers
         {
             var rsa = new RSACryptoServiceProvider();
             var keyBytes = Convert.FromBase64String(publicKey);
-            var asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
+            var x509Certificate = new X509CertificateParser().ReadCertificate(keyBytes);
+            var asymmetricKeyParameter = x509Certificate.GetPublicKey();
             var rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
             var rsaParameters = new RSAParameters
             {
